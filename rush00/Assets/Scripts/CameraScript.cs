24a4e404f8b52c6561246e560a1c56e90eb7541f
Razshal <fontenille.mathieu@gmail.com>
2019-05-25 12:09:19 +0200

    playerScript - rotation working

--- rush00/Assets/Scripts/CameraScript.cs
+++ rush00/Assets/Scripts/CameraScript.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour {
+public class CameraScript : MonoBehaviour
+{
     public GameObject player;
+    public Texture2D texture;
 
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+        if (texture)
+            Cursor.SetCursor(texture, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         gameObject.transform.position
                   = new Vector3(player.transform.position.x, player.transform.position.y, -10);
-	}
+    }
 }
--- rush00/Assets/Scripts/PlayerScript.cs
+++ rush00/Assets/Scripts/PlayerScript.cs
@@ -2,25 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
     public float movementSpeed = 0.2f;
+    private Vector3 relativeTarget;
+    private float rotationRadians;
+    private GameObject bodyContainer;
+    public enum State
+    {
+        STAY,
+        MOVING,
+        ATTACKING
+    }
+    public State state = State.STAY;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        bodyContainer = gameObject.transform.GetChild(0).gameObject;
+    }
 
-	void FixedUpdate()
-	{
+    void FixedUpdate()
+    {
         gameObject.transform.Translate(
-            new Vector3(Input.GetAxis("Horizontal") * movementSpeed, 
-                        Input.GetAxis("Vertical") * movementSpeed, 
+            new Vector3(Input.GetAxis("Horizontal") * movementSpeed,
+                        Input.GetAxis("Vertical") * movementSpeed,
                         0)
         );
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotation caclulation
+        relativeTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        rotationRadians = Mathf.Atan2(relativeTarget.y, relativeTarget.x) * Mathf.Rad2Deg - 90;
+        bodyContainer.transform.rotation = Quaternion.Euler(0f, 0f, rotationRadians);
+
+        if (!gameObject.GetComponent<Rigidbody2D>().velocity.Equals(Vector2.zero))
+            state = State.MOVING;
+    }
 }
