bccdc44483c7937836ca6ef0dd591db8ec47b8b7
Razshal <fontenille.mathieu@gmail.com>
2019-05-25 11:19:43 +0200

    init - syncing both remotes

---
+++ rush00/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public GameObject player;
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position
+                  = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+	}
+}
---
+++ rush00/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    public float movementSpeed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate()
+	{
+        gameObject.transform.Translate(
+            new Vector3(Input.GetAxis("Horizontal") * movementSpeed, 
+                        Input.GetAxis("Vertical") * movementSpeed, 
+                        0)
+        );
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
